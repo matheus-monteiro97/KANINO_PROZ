9c0c66666274c88719dabebd6a8c8e7d54fb603b
Ariel Velasco <arielvelascoalves@gmail.com>
2023-09-25 13:37:57 -0300

    Atualização geral aparência do site
    home atualizado

---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(`.carousel`).slick({
+        //slidesToShow: 1, // Quantidade de itens visíveis ao mesmo tempo
+        //slidesToScroll: 3, // Quantidade de itens a serem rolados
+        autoplay: true, // Ativar o modo de reprodução automática
+        autoplaySpeed: 3000, // Intervalo de tempo entre os slides (em milissegundos)
+    });
+});
